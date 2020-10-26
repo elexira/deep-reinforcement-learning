c02eb056b46e2a94492c536e2074bad8da2bbf4a
pemfir <pemfir@yahoo.com>
2020-10-25 22:21:39 -0700

    set up the env locally

---
+++ Pong_reinforce/readMe.md
@@ -0,0 +1,3 @@
+* to set-up the env run `sh setup.sh`
+* then activate `conda activate pong-env`
+* Finally lauanch the jupyter `nohup jupyter-notebook > jupyter.logs &`
---
+++ Pong_reinforce/setup.sh
@@ -0,0 +1,8 @@
+. ~/anaconda3/etc/profile.d/conda.sh
+conda env create -f pong.yml
+conda activate pong-env
+sudo apt install libcurl4-openssl-dev libssl-dev
+pip install -r requirement.txt
+
+
+
